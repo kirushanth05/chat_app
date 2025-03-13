83c4e3a1d3a3f67769093485e2c0f30b4ab8e475
kirushanth05 <kirushanthvijay@gmail.com>
2025-03-13 13:24:51 +0530

    main.dart configured to show the login/home screen based on the auth status.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,28 @@
 import 'package:chat_app/auth/auth.dart';
+import 'package:chat_app/pages/home.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(home: AuthScreen()));
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(
+    MaterialApp(
+      home: StreamBuilder(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          // if (snapshot.connectionState == ConnectionState.waiting) {
+          //   // show progress indicator
+          //   // show a splash screen
+          // }
+          if (snapshot.hasData) {
+            return HomeScreen();
+          }
+
+          return AuthScreen();
+        },
+      ),
+    ),
+  );
+}
